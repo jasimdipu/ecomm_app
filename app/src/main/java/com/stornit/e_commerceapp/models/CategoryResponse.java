3002eb806813ab660ea8232c5e31fff24e2b9085
dipu <dipu.j247@gmail.com>
2020-03-02 18:44:57 +0600

    Category Repository created

--- app/src/main/java/com/stornit/e_commerceapp/models/CategoryResponse.java
+++ app/src/main/java/com/stornit/e_commerceapp/models/CategoryResponse.java
@@ -19,10 +19,35 @@ public class CategoryResponse {
     private int totalPages;
 
 
+    public int getPage(){
+        return page;
+    }
 
+    public void setPage(int page) {
+        this.page = page;
+    }
 
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
 
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
 
+    public int getTotalResults() {
+        return totalResults;
+    }
 
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
 
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
 }
